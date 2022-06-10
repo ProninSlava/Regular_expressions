7a0b4f36f71de08e25da5d52d3f5b13683d579f1
Pronin Slava <prosnin-v@bk.ru>
2022-06-10 12:01:16 +0300

    2

--- main.py
+++ main.py
@@ -11,9 +11,14 @@ info = sequence(contacts_list)
 info_list = diblicate(info)
 
 for items in info_list[1:]:
-  pattern = r'(\+7|8)(\s*|\()(\(|s*)(495)(-|\)|s*)\s*(\d{3})(-|s*)(\d{2})(-|s*)(\d{2})(\s*|)(\(|\s*)(\доб.|)\s*(\d+|)(\)|)'
-  res = re.sub(pattern, r"+7(\4)\6-\8-\10 \13 \14", items[5])
-  items[5] = res
+  if 'доб' in items[5]:
+    pattern_d = r'(\+7|8)(\s*|\()(\(|s*)(495)(-|\)|s*)\s*(\d{3})(-|s*)(\d{2})(-|s*)(\d{2})(\s*|)(\(|\s*)(\доб.|)\s*(\d+|)(\)|)'
+    res = re.sub(pattern_d, r"+7(\4)\6-\8-\10 \13 \14", items[5])
+    items[5] = res
+  else:
+    pattern = r'(\+7|8)(\s*|\()(\(|s*)(495)(-|\)|s*)\s*(\d{3})(-|s*)(\d{2})(-|s*)(\d{2})'
+    res = re.sub(pattern, r"+7(\4)\6-\8-\10", items[5])
+    items[5] = res
 
 with open("phonebook.csv", "w") as f:
   datawriter = csv.writer(f, delimiter=',')
